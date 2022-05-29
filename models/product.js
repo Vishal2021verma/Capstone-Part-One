3c6dee7c61e9bf54eb1d1fc77ebd056deea687ae
Vishal Verma <89707084+Vishal2021verma@users.noreply.github.com>
2022-05-29 16:15:04 +0530

    corrected product model

--- models/product.js
+++ models/product.js
@@ -1,15 +1,16 @@
 const mongoose =require("mongoose");
 //product Schema
 const productSchema = mongoose.Schema({
-    id: {type: Number},
-    city: {type: String},
-    landmark: {type: String},
-    name: {type: String},
-    phone: {type: String},
-    state: {type: String},
-    street: {type: String},
-    zipcode: {type: String},
-    user_id: {type: String}
+    product_id:{type: Number},
+    avavilable_items: {type: Number},
+    category: {type:String},
+    created: {tpye: Date},
+    description: {type:String},
+    image_url: {type: String},
+    manufacturer: {type:String},
+    name:{type:String},
+    price: {type:Number},
+    updated: {type: Date}
 });
 //Product mongoose model
 const Product  =    mongoose.model("Product", productSchema, "products");

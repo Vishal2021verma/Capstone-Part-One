f90d7754415483436fdfba848d9ef126541f16eb
Vishal Verma <89707084+Vishal2021verma@users.noreply.github.com>
2022-05-29 16:09:39 +0530

    added address model

---
+++ models/address.js
@@ -0,0 +1,17 @@
+const mongoose =require("mongoose");
+//address Schema
+const addressSchema = mongoose.Schema({
+    id: {type: Number},
+    city: {type: String},
+    landmark: {type: String},
+    name: {type: String},
+    phone: {type: String},
+    state: {type: String},
+    street: {type: String},
+    zipcode: {type: String},
+    user_id: {type: String}
+});
+//Address mongoose model
+const Address  = mongoose.model("Address", productSchema, "addresses");
+
+module.exports = Address;

b5aaaff13c0aaeb078156604d102d21d2a19d794
Vishal Verma <89707084+Vishal2021verma@users.noreply.github.com>
2022-05-29 15:54:34 +0530

    added model Order

--- models/order.js
+++ models/order.js
@@ -1,6 +1,15 @@
 const mongoose = require("mongoose");
 
+//Order Schema
 const orderSchema = mongoose.Schema({
     id:mongoose.Schema.Types.ObjectId,
-    
+    amount: {type: double},
+    order_date: {type: Date},
+    product_product_id: {type: Number},
+    shiping_address_id: {type: Number},
+    user_id: {type: Number}
 });
+//Order Mongoose model 
+const Order= monggose.model("Order", orderSchema,"orders");
+
+module.export = Order;
